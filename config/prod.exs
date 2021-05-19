b954eb6760ff3dbb3cd8bc2d8ae86d6dbf1bd747
Edison Yap <edisonywh@gmail.com>
2021-05-19 16:23:39 +0200

    Fix websocket endpoint scheme

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :moon, MoonWeb.Endpoint,
-  url: [host: "gigalixirapp.com", port: 80],
+  url: [host: "backoffice-demo.gigalixirapp.com", port: 80, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
